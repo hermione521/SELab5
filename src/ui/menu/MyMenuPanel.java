947e67b31f19c1dd3e97be0d3df9540f708f45ce
shiyinayuriko <whitecometliliwill@gmail.com>
2014-06-12 01:54:44 +0800

    UI实现

--- src/ui/menu/MyMenuPanel.java
+++ src/ui/menu/MyMenuPanel.java
@@ -43,7 +43,7 @@ public class MyMenuPanel extends MyAbstractJPanel implements MenuPannel {
 		buttonStart.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				mActions.clickStartConfig(listPanel.getList().getSelectedValue());
+				mActions.clickStartConfig(listPanel.getList().getSelectedIndex());
 			}
 		});
 		
